49b9dcd1faa52347da227d4c20fcb59b31bb450c
ajyng <zxzimin@naver.com>
2021-03-04 18:25:30 +0900

    [feat] User 모델 구현

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
+from django.shortcuts import resolve_url
 
-# Create your models here.
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+    
+    follower_set = models.ManyToManyField("self", blank=True)
+    following_set = models.ManyToManyField("self", blank=True)
+
+    website_url = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices)
+    avatar = models.ImageField(blank=True, upload_to="accounts/avatar/%Y/%m/%d",
+                               help_text="48px * 48px 크기의 png/jpg 파일을 업로드해주세요.")
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+    
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        else:
+            return resolve_url("pydenticon_image", self.username)
--- workout/settings.py
+++ workout/settings.py
@@ -19,12 +19,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third apps
+
+    # local apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +74,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -106,3 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
