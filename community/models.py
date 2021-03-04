bb19838dc5fac1d616571918a22c864fa6d43c1a
ajyng <zxzimin@naver.com>
2021-03-04 18:44:46 +0900

    [feat] Post 모델 구현

--- community/models.py
+++ community/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # def get_absolute_url(self):
+    #     return reverse("community:post_detail", args=[self.pk])
--- community/views.py
+++ community/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+def post_new(request):
+    pass
+
+def post_list(request):
+    pass
+
+def post_detail(request):
+    pass
+
+def post_update(request):
+    pass
+
+def post_delete(request):
+    pass
+
+def post_like(request):
+    pass
