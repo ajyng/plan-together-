ea937c79c099d0f1dcf5c6d48a0f1f80caff86c8
ajyng <zxzimin@naver.com>
2021-03-06 03:25:31 +0900

    [feat] Login API 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
 from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
@@ -6,11 +6,11 @@ from .forms import SignupForm
 
 class SignupView(CreateView):
     form_class = SignupForm
-    template_name = 'accounts/signup_form.html'
+    template_name = 'accounts/user_form.html'
     success_url = settings.LOGIN_URL
 
-def login(request):
-    pass
+class LoginView(LoginView):
+    template_name = 'accounts/user_form.html'
 
 def profile(request):
     pass
