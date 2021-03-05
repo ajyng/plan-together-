838afadbd53cee54e84423276f068a2bb8b908a7
ajyng <zxzimin@naver.com>
2021-03-06 05:07:39 +0900

    [feat] Logout API 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path, include
+from django.urls import path, re_path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('<username>/', views.ProfileView.as_view(), name='profile'),
     path('signup/', views.SignupView.as_view(), name='signup'),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name = 'accounts/user_form.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    re_path(r'^(?P<username>[\w.@+-]+)/$', views.ProfileView.as_view(), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,20 @@
-from django.shortcuts import render, resolve_url, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView, DetailView
 from .forms import SignupForm
+
+User = get_user_model()
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = 'accounts/user_form.html'
     success_url = settings.LOGIN_URL
 
-class LoginView(LoginView):
-    template_name = 'accounts/user_form.html'
-
 class ProfileView(DetailView):
-    model = get_user_model()
+    model = User
     template_name = 'accounts/profile.html'
 
-    def get_object(self, queryset=None):
-        object = get_object_or_404(get_user_model(), username=self.kwargs['username'])
-        return object
+    def get_object(self):
+        username = self.kwargs.get('username')
+        return get_object_or_404(User, username=username)
